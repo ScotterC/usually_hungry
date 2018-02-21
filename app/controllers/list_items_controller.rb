6fb54ee036f35230de247f3fa77888efa5a778ca
Scott Carleton <scott@artsicle.com>
2018-02-20 23:04:12 -0500

    Cleaning up views and tests
    Added a users controller to show users page

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -1,11 +1,6 @@
 class ListItemsController < ApplicationController
   before_action :set_list
-  before_action :set_list_item, only: [:show, :edit, :update, :destroy]
-
-  # GET /list/1/items
-  def index
-    @list_items = ListItem.all
-  end
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   # GET /list/1/items/1
   def show
@@ -13,38 +8,38 @@ class ListItemsController < ApplicationController
 
   # GET /list/1/items/new
   def new
-    @list_item = ListItem.new
+    @item = ListItem.new
   end
 
-  # GET /list_items/1/edit
+  # GET /items/1/edit
   def edit
   end
 
-  # POST /list_items
+  # POST /items
   def create
     # params[:list_id]
-    @list_item = ListItem.new(list_item_params)
+    @item = ListItem.new(item_params)
 
-    if @list_item.save
-      redirect_to @list, notice: 'List item was successfully created.'
+    if @item.save
+      redirect_to @list, notice: 'Item was successfully created.'
     else
       render :new
     end
   end
 
-  # PATCH/PUT /list_items/1
+  # PATCH/PUT /items/1
   def update
-    if @list_item.update(list_item_params)
-      redirect_to [@list, @list_item], notice: 'List item was successfully updated.'
+    if @item.update(item_params)
+      redirect_to list_path(@list), notice: 'Item was successfully updated.'
     else
       render :edit
     end
   end
 
-  # DELETE /list_items/1
+  # DELETE /items/1
   def destroy
-    @list_item.destroy
-    redirect_to list_list_items_url(@list), notice: 'List item was successfully destroyed.'
+    @item.destroy
+    redirect_to list_url(@list), notice: 'Item was successfully destroyed.'
   end
 
   private
@@ -53,12 +48,12 @@ class ListItemsController < ApplicationController
     end
 
     # Use callbacks to share common setup or constraints between actions.
-    def set_list_item
-      @list_item = ListItem.find(params[:id])
+    def set_item
+      @item = ListItem.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
-    def list_item_params
+    def item_params
       params.require(:list_item).permit(:list_id, :name, :description)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  resources :users, only: [:show]
+
   resources :lists do
-    resources :list_items, path: :items
+    resources :items, controller: "list_items", except: :index
   end
 
 
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "pages#index"
